f0796415e2b74547519bf213db5f8849bd3b3bfa
rattapon simsri <rattapon.simsri0@gmail.com>
2021-02-15 14:58:45 +0700

    search pantip csv

--- app.js
+++ app.js
@@ -4,35 +4,35 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
-let _path = path.join(__dirname, "team.csv");
+let _path = path.join(__dirname, "export_dataframe.csv");
 
-const raw = fs.readFileSync(_path, "utf8");
 const readCSV = async () => {
+  const raw = fs.readFileSync(_path, "utf8");
   const result = await csv(raw);
   return result;
 };
 
 app.get("/", async (req, res) => {
   const data = await readCSV();
-  res.json({ result : data });
+  res.json({ result: data });
 });
 
 const search = async (toSearch) => {
   var results = [];
   const data = await readCSV();
   for (var i = 0; i < data.length; i++) {
-    if (data[i]["common_name"].toLowerCase().indexOf(toSearch) != -1) {
+    if (data[i]["title"].toLowerCase().indexOf(toSearch) != -1) {
       results.push(data[i]);
     }
   }
   return results;
 };
 
-app.get("/team/", async (req, res) => {
-  const name =  req.query.name
-  const results = await search(name.toLowerCase())
-  console.log(results.length)
-  res.json({data : results});
+app.get("/keywords", async (req, res) => {
+  const title = req.query.title;
+  const results = await search(title.toLowerCase());
+  // console.log(results.length);
+  res.json({ data: results });
 });
 
 app.listen(3000, () => {
